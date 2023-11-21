bfd4adaafd2d16fb2284c4a680bf5cc4863ecea0
BlackspearZero <sean.platford@gmail.com>
2023-11-20 22:19:47 -0800

    Day 5 - 1

--- Game1/maingame.py
+++ Game1/maingame.py
@@ -6,12 +6,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+test_surface = pygame.image.load('graphics/Sky.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    #draw all our elements
-    #update everything
+
+    screen.blit(test_surface,(200,100))
+
     pygame.display.update()
     clock.tick(60)
