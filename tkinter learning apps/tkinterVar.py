af0920e3e2461d84816b3be9b9d64d70a5a62dce
Sean Platford <sean.platford@gmail.com>
2024-01-14 11:00:00 -0800

    Day 59

--- tkinter learning apps/tkinterVar.py
+++ tkinter learning apps/tkinterVar.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 
+def button_func():
+    print(string_var.get())
+    string_var.set('button pressed')
+
 window = tk.Tk()
 window.title('Tkinter Variables')
 
@@ -11,5 +15,8 @@ label.pack()
 
 entry = ttk.Entry(master = window, textvariable = string_var)
 entry.pack()
+
+button = ttk.Button(master = window, text = 'Button', command = button_func)
+button.pack()
 
 window.mainloop()
